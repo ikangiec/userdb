db42afd0dfbdbaf608e588de6510057910d1578f
K C <aries0420@gmail.com>
2013-09-09 00:44:47 -0700

    Homework 5.1 for buiding a user database

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Userdb::Application.config.secret_key_base = '4f17b5f34c85e6cef7cd121aa52f1df90732d985bb25846cc43085e9a7980cf43fb47d6c9f1ef25bfd6e650b0ea922cf4edba4d985a50ffddcf9ffb0366789c3'
